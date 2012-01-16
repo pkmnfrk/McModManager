155da001fd4176c06f290df6891edc14a4a94348
Mike Caron <caron.mike@gmail.com>
2012-01-15 19:40:56 -0500

    initial checkin, with SharpZipLib

---
+++ AppData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCModManager {
+    internal static class AppData {
+        private static Archive pureMinecraftJar;
+        public static Archive PureMinecraftJar {
+            get {
+                return pureMinecraftJar;
+            }
+        }
+
+        public static void InitAppData() {
+
+        }
+    }
+}
---
+++ Archive.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ICSharpCode.SharpZipLib;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace MCModManager {
+    public class Archive {
+        private ZipFile zip;
+
+        public Archive(string file) {
+            zip = new ZipFile(file);
+        }
+    }
+}
