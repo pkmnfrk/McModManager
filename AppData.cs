1168ba7c0c802060333c325f9d041e83265e9162
Mike Caron <caron.mike@gmail.com>
2012-01-15 20:13:25 -0500

    Start work on manifests

--- AppData.cs
+++ AppData.cs
@@ -2,17 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace MCModManager {
     internal static class AppData {
-        private static Archive pureMinecraftJar;
-        public static Archive PureMinecraftJar {
+
+        private Dictionary<string, Mod> mods = new Dictionary<string, Mod>();
+        public Dictionary<string, Mod> Mods {
+            get { return mods; }
+        }
+
+        private static string AppDataPath {
             get {
-                return pureMinecraftJar;
+                return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MCModManager");
             }
         }
 
+        private static string ArchivesPath {
+            get {
+                return Path.Combine(AppDataPath, "archives");
+            }
+        }
+
+        private static string ModArchivePath(string mod) {
+            return Path.Combine(ArchivesPath, mod);
+        }
+
         public static void InitAppData() {
+
+            if (!Directory.Exists(AppDataPath)) {
+                Directory.CreateDirectory(AppDataPath);
+            }
+
+            if (!Directory.Exists(ArchivesPath)) {
+                Directory.CreateDirectory(ArchivesPath);
+            }
+
+            //minecraft.jar is special, so lets load it now
+            
+
+            
 
         }
     }
---
+++ Manifest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Net;
+
+namespace MCModManager {
+    public class Manifest {
+        public string Name { get; set; }
+        public IList<Version> Versions { get; protected set; }
+
+        private const XNamespace ns = XNamespace.Get("http://mike-caron.com/McModManager/manifest");
+
+        public static Manifest LoadFromUrl(string uri) {
+            XDocument manifest = XDocument.Load(uri);
+
+            if (manifest.Root.Name != ns.GetName("manifest")) {
+                throw new Exception("That's not a McModManager manifest");
+            }
+
+            Manifest ret = new Manifest();
+
+            ret.Name = manifest.Root.Element(ns.GetName("name")).Value;
+
+            return ret;
+        }
+
+        public class Version {
+            public string URL { get; set; }
+            public string Version { get; set; }
+            public PackingType Packing { get; set; }
+
+            public enum PackingType {
+                Unknown,
+                ModLoader,
+                Raw
+            }
+        }
+    }
+}
