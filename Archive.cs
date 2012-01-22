27697bf39f2cc6e39c606747ef9d00d065e2836f
Mike Caron <caron.mike@gmail.com>
2012-01-21 23:02:59 -0500

    Sincerest regret to github's servers...

--- Archive.cs
+++ Archive.cs
@@ -1,6 +1,11 @@
-﻿namespace MCModManager
-{
+﻿//-----------------------------------------------------------------------
+// <copyright file="Archive.cs" company="Mike Caron">
+//     using System.Standard.Disclaimer;
+// </copyright>
+//-----------------------------------------------------------------------
 
+namespace MCModManager
+{
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -9,13 +14,23 @@
     using ICSharpCode.SharpZipLib;
     using ICSharpCode.SharpZipLib.Zip;
 
+    /// <summary>
+    /// A helper class for dealing with Zip/Jar files
+    /// </summary>
     public class Archive
     {
+        /// <summary>
+        /// The zip file
+        /// </summary>
         private ZipFile zip;
 
+        /// <summary>
+        /// Initializes a new instance of the Archive class
+        /// </summary>
+        /// <param name="file">the file to open</param>
         public Archive(string file)
         {
-            zip = new ZipFile(file);
+            this.zip = new ZipFile(file);
         }
     }
 }
