174189cf6aa2cb8f043bdb3dd63bbb30a461ecb0
Mike Caron <caron.mike@gmail.com>
2012-01-22 00:18:11 -0500

    Fix Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,42 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Form1.cs" company="Mike Caron">
+//     using System.Standard.Disclaimer;
+// </copyright>
+//-----------------------------------------------------------------------
 
-namespace MCModManager {
-    public partial class Form1 : Form {
-        public Form1() {
+namespace MCModManager
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The main window
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the Form1 class
+        /// </summary>
+        public Form1()
+        {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
+        private void Form1_Load(object sender, EventArgs e)
+        {
             treeView1.BeginUpdate();
-            foreach (var mod in AppData.Mods.Values) {
-                var tvwMod = new TreeNode(mod.Name, new [] {
-                        new TreeNode("ID: " + mod.Id),
-                        new TreeNode("Url: " + mod.Url),
-                });
+            foreach (var mod in AppData.Mods.Values)
+            {
+                var tvwModSubnodes = new[]
+                {
+                    new TreeNode("ID: " + mod.Id),
+                    new TreeNode("Url: " + mod.Url),
+                };
+
+                var tvwMod = new TreeNode(mod.Name, tvwModSubnodes);
                 var tvwModVer = new TreeNode("Versions:");
                 tvwMod.Nodes.Add(tvwModVer);
 
-                foreach (var ver in mod.Versions) {
-                    var tvwVer = new TreeNode(ver.Ver, new [] {
+                foreach (var ver in mod.Versions)
+                {
+                    var tvwVerSubnodes = new[]
+                    {
                         new TreeNode("Url: " + ver.Url),
                         new TreeNode("Packing: " + ver.Packing),
-                    });
+                    };
+
+                    var tvwVer = new TreeNode(ver.Ver, tvwVerSubnodes);
                     var tvwVerDep = new TreeNode("Dependencies:");
                     tvwVer.Nodes.Add(tvwVerDep);
 
-                    foreach (var dep in ver.Dependencies) {
+                    foreach (var dep in ver.Dependencies)
+                    {
                         var tvwDep = new TreeNode(dep.ToString());
                         tvwVerDep.Nodes.Add(tvwDep);
                     }
 
-                    if (tvwVerDep.Nodes.Count == 0) {
+                    if (tvwVerDep.Nodes.Count == 0)
+                    {
                         tvwVerDep.Nodes.Add(new TreeNode("None") { NodeFont = new Font(treeView1.Font, FontStyle.Italic) });
                     }
 
@@ -45,6 +71,7 @@ namespace MCModManager {
 
                 treeView1.Nodes.Add(tvwMod);
             }
+
             treeView1.EndUpdate();
         }
     }
